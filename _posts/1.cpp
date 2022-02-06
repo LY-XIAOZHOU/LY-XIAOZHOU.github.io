a65fad57adc96ed25e42a798c5605123d9356c21
LY <1647609961@qq.com>
2022-02-06 22:09:03 +0800

    1

--- _posts/1.cpp
+++ _posts/1.cpp
@@ -6,12 +6,11 @@ using namespace std;
 
 int main()
 {
-    string a = "{hello_world}";
-    int  len = a.size();
-    for(int i = 0 ; i < len ; i++)
-    {
-        if(int(a[i])==111)
-            a[i]=char(48);
-    }
-    cout<<"flag="<<a;
+    string a ="f\nk\fw&O.@\x11x\rZ;U\x11p\x19";
+    string c = "F\x1Fv\"M#D\x0Eg\x06h\x0FG2O";
+    string b = a + c;
+    int len = b.size() ; 
+   for (int i = len -1; i>0; i-- )
+    b[i] ^= b[i-1];
+    cout<<"flag="<<b;
 }
